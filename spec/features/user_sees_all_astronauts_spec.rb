c4d4f5d53a42a1b2295dd10cfe5dc9fd8fa132eb
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-08-24 11:47:37 -0600

    ATP adds average age astronaut index with index spec test, adds instance variable to index route for average age

--- spec/features/user_sees_all_astronauts_spec.rb
+++ spec/features/user_sees_all_astronauts_spec.rb
@@ -7,13 +7,24 @@ describe "user sees all astronauts" do
       astro_2 = Astronaut.create!(name: "Ella", age: 29, job: "Science Officer")
 
       visit '/astronauts'
-    
+
       expect(page).to have_content(astro_1.name)
       expect(page).to have_content(astro_1.age)
       expect(page).to have_content(astro_1.job)
       expect(page).to have_content(astro_2.name)
       expect(page).to have_content(astro_2.age)
       expect(page).to have_content(astro_2.job)
+    end
+
+    it "displays average age" do
+      astro_1 = Astronaut.create!(name: "Jane", age: 31, job: "Commander")
+      astro_2 = Astronaut.create!(name: "Ella", age: 29, job: "Science Officer")
+
+      visit '/astronauts'
+      save_and_open_page
+      
+
+      expect(page).to have_content("Average Astronaut Age: #{Astronaut.avg_age}")
     end
   end
 end
