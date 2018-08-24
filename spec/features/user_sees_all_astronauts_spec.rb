abd9c613e1d4962589a28be64018e4964db60cbe
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-08-24 11:23:45 -0600

    corrects spec error

--- spec/features/user_sees_all_astronauts_spec.rb
+++ spec/features/user_sees_all_astronauts_spec.rb
@@ -3,10 +3,12 @@ require 'rails_helper'
 describe "user sees all astronauts" do
   describe "they visit /astronauts" do
     it "displays all astronauts" do
-      astro_1 = Article.create!(name: "Jane", age: 31, job: "Commander")
-      astro_2 = Article.create!(name: "Ella", age: 29, job: "Science Officer")
+      astro_1 = Astronaut.create!(name: "Jane", age: 31, job: "Commander")
+      astro_2 = Astronaut.create!(name: "Ella", age: 29, job: "Science Officer")
 
       visit '/astronauts'
       expect(page).to have_content(astro_1.name)
       expect(page).to have_content(astro_2.name)
     end
+  end
+end
